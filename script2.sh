7da5e5fd416bf122039f95fd1ab7fcd0c8dc3c68
Pavel Voronin <frostlin@pop-os.localdomain>
2021-09-12 22:44:47 +0300

    added scheduling instructions

--- script2.sh
+++ script2.sh
@@ -7,5 +7,11 @@ for num in $(seq $num); do
   cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 1000 | head -n 1 > $filename
 done
 
+# To schedule this script we need to use cron
+# 1. Open crontab with $crontab -e
+# 2. Write following line: 
+#        0 */6 * * * /home/frostlin/script2.sh
+# this line sets script to run every 6 hours
+
 
 
